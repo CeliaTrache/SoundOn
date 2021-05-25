020ad3906aba8248b895ae4e75838b44b4bdc700
CeliaTrache <celiatrache@gmail.com>
2021-05-25 17:06:15 +0200

    fix game_policy and authorize @game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,13 @@
 class GamesController < ApplicationController
   def new
     @game = Game.new
+    authorize @game
   end
 
   def create
     @game = Game.new(game_params)
     @game.user = current_user
+    authorize @game
     if @game.save
       redirect_to edit_game_path(@game)
     else
@@ -15,16 +17,20 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    authorize @game
   end
 
   def edit
     @number_of_players = params[:number_of_players].to_i if params[:number_of_players].present?
     @game = Game.find(params[:id])
+    authorize @game
     @game.players.build
   end
 
   def update
-    @game = Game.update(game_params)
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    authorize @game
     redirect_to game_path(@game)
   end
 
