a9318a002a31c9ba482e0bae5704442b2ef3a255
CeliaTrache <celiatrache@gmail.com>
2021-05-25 17:52:01 +0200

    uncomment test/spotify.rb

--- app/tests/spotify.rb
+++ app/tests/spotify.rb
@@ -1,18 +1,18 @@
-# require 'rspotify'
+require 'rspotify'
 
-# RSpotify.authenticate("2f6e9a91424245a4ba492468eccc9ec3", "51f213d73aa947e88ad9a7a886b233c7")
+RSpotify.authenticate("2f6e9a91424245a4ba492468eccc9ec3", "51f213d73aa947e88ad9a7a886b233c7")
 
-# # celia = RSpotify::User.find('21qmhgd3whycge7hownsdhpea')
+# celia = RSpotify::User.find('21qmhgd3whycge7hownsdhpea')
 
-# # p celia
+# p celia
 
-# # playlist = celia.playlists
+# playlist = celia.playlists
 
-# # p playlist.class
+# p playlist.class
 
-# playlists = RSpotify::Playlist.search('Indie')
+playlists = RSpotify::Playlist.search('Indie')
 
-# playlist = playlists.first
-# p playlist.tracks.first.preview_url
+playlist = playlists.first
+p playlist.tracks.first.preview_url
 
 
