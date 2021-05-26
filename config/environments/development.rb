46d79162ff26583f4f6e2c02fa4d829daaf04633
julien loiseau <julien.loiseau@edu.escp.eu>
2021-05-26 15:18:10 +0200

    Added configuration requirement for ngrock

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # ngrock configuration
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
