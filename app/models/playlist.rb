7239198552cb8596c72a7d77f75880bdf4bfcc42
vincentvinciguerra <vincent.vinciguerra@outlook.fr>
2021-05-27 14:58:15 +0200

    playlist-view-2

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,8 @@
 class Playlist < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description_and_category,
+    against: [ :title, :description, :category ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
