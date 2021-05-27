d29fd6cc6230d2082a75a59f01bfd31e521bd2f0
vincentvinciguerra <vincent.vinciguerra@outlook.fr>
2021-05-27 11:52:21 +0200

    home-page-modif

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get :results
     end
   end
-  resources :playlist, only: [:index]
+  resources :playlists, only: [:index]
   resources :players, only: [:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
