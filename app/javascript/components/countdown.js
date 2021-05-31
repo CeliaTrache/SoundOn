b5347e9a2f6ffb88ef8c53b38bcbfe13cf2aa858
CeliaTrache <celiatrache@gmail.com>
2021-05-31 15:32:38 +0200

    le jeu crée autant de chanson qu'il faut pour la durée du jeu, mais bug sur les chansons lues deux fois

--- app/javascript/components/countdown.js
+++ app/javascript/components/countdown.js
@@ -23,36 +23,42 @@ const countdown = () => {
   let timerInterval = null;
   let remainingPathColor = COLOR_CODES.info.color;
 
-  document.getElementById("app").innerHTML = `
-  <div class="base-timer">
-    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g class="base-timer__circle">
-        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <path
-          id="base-timer-path-remaining"
-          stroke-dasharray="283"
-          class="base-timer__path-remaining ${remainingPathColor}"
-          d="
-            M 50, 50
-            m -45, 0
-            a 45,45 0 1,0 90,0
-            a 45,45 0 1,0 -90,0
-          "
-        ></path>
-      </g>
-    </svg>
-    <span id="base-timer-label" class="base-timer__label" style="color: white">${formatTime(
-      timeLeft
-    )}</span>
-  </div>
-  `;
+  const app = document.getElementById("app");
+  if (app) {
+    app.innerHTML = `
+    <div class="base-timer">
+      <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <g class="base-timer__circle">
+          <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+          <path
+            id="base-timer-path-remaining"
+            stroke-dasharray="283"
+            class="base-timer__path-remaining ${remainingPathColor}"
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+          ></path>
+        </g>
+      </svg>
+      <span id="base-timer-label" class="base-timer__label" style="color: white">${formatTime(
+        timeLeft
+      )}</span>
+    </div>
+    `;
+  }
+
 
   // Button ID
   const startLoc = document.getElementById('start')
-  
-  startLoc.addEventListener('click', (event) => {
-    startTimer();
-  })
+
+  if (startLoc) {
+    startLoc.addEventListener('click', (event) => {
+      startTimer();
+    })
+  }
 
   function onTimesUp() {
     clearInterval(timerInterval);
@@ -68,7 +74,8 @@ const countdown = () => {
       setCircleDasharray();
       setRemainingPathColor(timeLeft);
 
-      if (timeLeft === 0) {
+      // "==" vs "==="
+      if (timeLeft == 0) {
         onTimesUp();
       }
     }, 1000);
--- app/javascript/components/init_player.js
+++ app/javascript/components/init_player.js
@@ -23,78 +23,88 @@ const play = ({
 const initPlayer = () => {
   window.onSpotifyWebPlaybackSDKReady = () => {
     // You can now initialize Spotify.Player and use the SDK
-    
     // Retrieve token
-    const tokenLoc = document.getElementById('player')
-    const token = tokenLoc.dataset.token
-    console.log(token)
+    const tokenLoc = document.getElementById('player');
+    if (tokenLoc) {
+      let token = tokenLoc.dataset.token;
+      // console.log(token);
+      // Retrieve track uri
+      // console.log(track)
+      // Buttons identifications
+      const startLoc = document.getElementById('start')
+      const stopLoc = document.getElementById('stop')
+      const addTime = document.getElementById('time')
+      let timer = 15000
 
-    // Retrieve track uri
-    const trackLoc = document.getElementById('player')
-    const track = trackLoc.dataset.trackId
-    console.log(track)
-
-    // Buttons identifications
-    const startLoc = document.getElementById('start')
-    const stopLoc = document.getElementById('stop')
-    const addTime = document.getElementById('time')
-    let timer = 15000
-
-    // Define Spotify Player
-    let player = new Spotify.Player({
-      name: 'Blindtest Player',
-      getOAuthToken: callback => {
-        // Run code to get a fresh access token
-        callback(token);
-      },
-    });
-
-    // Error handling
-    player.addListener('initialization_error', ({ message }) => { console.error(message); });
-    player.addListener('authentication_error', ({ message }) => { console.error(message); });
-    player.addListener('account_error', ({ message }) => { console.error(message); });
-    player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-    // Playback status updates
-    player.addListener('player_state_changed', state => { console.log(state); });
-    
-    startLoc.addEventListener('click', (event) => {
-      console.log("ok");
-      player.connect().then(success => {
-        if (success) {
-          console.log('The Web Playback SDK successfully connected to Spotify!');
-        }
+      // Define Spotify Player
+      let player = new Spotify.Player({
+        name: 'Blindtest Player',
+        getOAuthToken: callback => {
+          // Run code to get a fresh access token
+          callback(token);
+        },
       });
-      player.addListener('ready', ({ device_id }) => {
-        console.log('The Web Playback SDK is ready to play music!');
-        console.log('Device ID', device_id);
-        play({
-          playerInstance: player,
-          spotify_uri: track,
-          device_id: device_id
-        });
 
-        // In progress : trying to add 15 seconds
-        addTime.addEventListener('click', (event) => {
-          player.addListener('player_state_changed', ({
-            position,
-            duration,
-            track_window: { current_track }
-          }) => {
-            console.log('Currently Playing', current_track);
-            console.log('Position in Song', position);
-            console.log('Duration of Song', duration);
+      // Error handling
+      player.addListener('initialization_error', ({ message }) => { console.error(message); });
+      player.addListener('authentication_error', ({ message }) => { console.error(message); });
+      player.addListener('account_error', ({ message }) => { console.error(message); });
+      player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+      // Playback status updates
+      player.addListener('player_state_changed', state => { console.log(state); });
+
+      if (startLoc) {
+        startLoc.addEventListener('click', (event) => {
+          console.log("ok");
+          player.connect().then(success => {
+            if (success) {
+              console.log('The Web Playback SDK successfully connected to Spotify!');
+            }
           });
-          timer = timer + 15000;
-          console.log('time increased');
-          console.log(timer)
+          player.addListener('ready', ({ device_id }) => {
+            console.log('The Web Playback SDK is ready to play music!');
+            console.log('Device ID', device_id);
+            let track = tokenLoc.dataset.trackId;
+            console.log(track)
+
+            play({
+              playerInstance: player,
+              spotify_uri: track,
+              device_id: device_id
+            })
+
+
+
+            // In progress : trying to add 15 seconds
+            addTime.addEventListener('click', (event) => {
+              player.addListener('player_state_changed', ({
+                position,
+                duration,
+                track_window: { current_track }
+              }) => {
+                console.log('Currently Playing', current_track);
+                console.log('Position in Song', position);
+                console.log('Duration of Song', duration);
+              });
+              timer = timer + 15000;
+              console.log('time increased');
+              console.log(timer)
+            })
+          });
+          setTimeout(function(){player.pause().then(() => {
+            console.log(timer);
+            console.log('Paused!');
+            player.disconnect().then(() => {
+              console.log('DECONNECTEEE!');
+              Turbolinks.clearCache()
+            });
+          })}, timer)
         })
-      });
-      setTimeout(function(){player.pause().then(() => {
-        console.log(timer);
-         console.log('Paused!');
-      })}, timer)
-    })
+      }
+    }
+
+
   };
 }
 
