d1040cdbb0ab6feccf5bbca26bdecafe515df157
CeliaTrache <celiatrache@gmail.com>
2021-06-03 16:53:26 +0200

    add my playlist divan du monde

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # Now you can access user's private data, create playlists and much more
     @user = User.find_or_initialize_by(email: @spotify_user.email)
     # Access private data
+    @user.spotify_token = @spotify_user.id
     @user.token = @spotify_user.credentials["token"]
     @user.save
     if @user.new_record?
