2724ee99b62be0777353edceebe35e2b888e20bf
Cindy Grenet <cindyyyy4@gmail.com>
2021-06-03 17:29:56 +0200

    Winner score ok

--- db/migrate/20210524145004_create_players.rb
+++ db/migrate/20210524145004_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
       t.string :nickname
-      t.integer :score
+      t.integer :score, default: 0
       t.references :game, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_104505) do
 
   create_table "players", force: :cascade do |t|
     t.string "nickname"
-    t.integer "score"
+    t.integer "score", default: 0
     t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
